a507f6a6f446f12a25f51085ef9a370cf6b2165b
Aniluffi <90216018+Aniluffi@users.noreply.github.com>
2023-12-13 09:54:32 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Cvartira.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Cvartira
+    {
+        int number;
+        int coast;
+        int s;
+
+        public int S 
+        {
+            set { if (value < 0) Console.WriteLine("Площадь не может быть меньше 0");
+                else s = value;
+            } 
+            get { return s; }
+        }
+
+        public int Coast 
+        {
+            get
+            {
+                return coast;
+            }
+            set
+            {
+                    if (value < 0) Console.WriteLine("Цена не может быть меньше 0");
+                    else coast = value;
+            }
+        }
+
+        public Cvartira ()
+        {
+
+        }
+
+        public Cvartira(int number,int coast,int s)
+        {
+            this.S = s;
+            this.number = number;
+            this.Coast = coast;
+        }
+
+        public void Inf ()
+        {
+            Console.WriteLine($"Информация о квартире:\n" +
+                $"Номер - {number}\n" +
+                $"Цена за 1 кв - {coast}\n" +
+                $"S - {s}\n");
+        }
+
+        public void CoastKv() 
+        {
+            Console.WriteLine(s*coast);
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp2;
+
+class Program 
+{ 
+    static void Main(string[] args)
+    {
+        Cvartira cv1 = new Cvartira(23,-30,-1);
+        cv1.Inf();
+        cv1.S = 12;
+        cv1.Coast = 100;
+        cv1.Inf();
+        cv1.CoastKv();
+    }
+}
